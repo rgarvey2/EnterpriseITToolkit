b0d1285745d64af8b4778513e54dcdf3730a626e
rgarvey2 <78882487+rgarvey2@users.noreply.github.com>
2025-09-22 12:00:08 -0400

    Fix: Simplify webpack configuration to resolve MODULE_NOT_FOUND error
    - Remove complex webpack plugins that were causing module not found errors
    - Simplify webpack.config.js to use only basic path and built-in webpack features
    - Add missing dependencies: style-loader and css-loader to dependencies
    - Remove TerserPlugin, CssMinimizerPlugin, MiniCssExtractPlugin dependencies
    - Use simple CSS loader configuration that works on Render
    - Fix build failure caused by missing webpack plugin modules

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,4 @@
 const path = require('path');
-const TerserPlugin = require('terser-webpack-plugin');
-const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = (env, argv) => {
     const isProduction = argv.mode === 'production';
@@ -23,78 +20,9 @@ module.exports = (env, argv) => {
             rules: [
                 {
                     test: /\.css$/,
-                    use: [
-                        MiniCssExtractPlugin.loader,
-                        'css-loader',
-                        {
-                            loader: 'postcss-loader',
-                            options: {
-                                postcssOptions: {
-                                    plugins: [
-                                        require('autoprefixer'),
-                                        require('cssnano')({
-                                            preset: 'default'
-                                        })
-                                    ]
-                                }
-                            }
-                        }
-                    ]
-                },
-                {
-                    test: /\.(png|jpg|jpeg|gif|svg|ico)$/,
-                    type: 'asset/resource',
-                    generator: {
-                        filename: 'images/[name].[contenthash][ext]'
-                    }
-                },
-                {
-                    test: /\.(woff|woff2|ttf|eot)$/,
-                    type: 'asset/resource',
-                    generator: {
-                        filename: 'fonts/[name].[contenthash][ext]'
-                    }
+                    use: ['style-loader', 'css-loader']
                 }
             ]
-        },
-        plugins: [
-            new MiniCssExtractPlugin({
-                filename: isProduction ? '[name].[contenthash].css' : '[name].css'
-            })
-        ],
-        optimization: {
-            minimize: isProduction,
-            minimizer: [
-                new TerserPlugin({
-                    terserOptions: {
-                        compress: {
-                            drop_console: isProduction,
-                            drop_debugger: isProduction
-                        },
-                        mangle: isProduction
-                    }
-                }),
-                new CssMinimizerPlugin()
-            ],
-            splitChunks: {
-                chunks: 'all',
-                cacheGroups: {
-                    vendor: {
-                        test: /[\\/]node_modules[\\/]/,
-                        name: 'vendors',
-                        chunks: 'all',
-                        priority: 10
-                    },
-                    common: {
-                        name: 'common',
-                        minChunks: 2,
-                        chunks: 'all',
-                        priority: 5,
-                        reuseExistingChunk: true
-                    }
-                }
-            },
-            runtimeChunk: 'single'
         },
         resolve: {
             extensions: ['.js', '.css'],
@@ -102,26 +30,6 @@ module.exports = (env, argv) => {
                 '@': path.resolve(__dirname, 'Web/wwwroot')
             }
         },
-        devtool: isProduction ? 'source-map' : 'eval-source-map',
-        devServer: {
-            static: {
-                directory: path.join(__dirname, 'Web/wwwroot'),
-                publicPath: '/'
-            },
-            compress: true,
-            port: 3000,
-            hot: true,
-            historyApiFallback: true,
-            headers: {
-                'Cache-Control': 'no-cache, no-store, must-revalidate',
-                'Pragma': 'no-cache',
-                'Expires': '0'
-            }
-        },
-        performance: {
-            hints: isProduction ? 'warning' : false,
-            maxEntrypointSize: 512000,
-            maxAssetSize: 512000
-        }
+        devtool: isProduction ? false : 'eval-source-map'
     };
 };
